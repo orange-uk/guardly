56df7af9290ac66e075ac36cbcd0fe8174b09ee5
orange-uk <93472932+orange-uk@users.noreply.github.com>
2026-05-30 01:14:52 +0100

    Update routing to include LandingPage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import React, { useState, useEffect } from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Dashboard from './pages/Dashboard'
 import ProfilePage from './pages/ProfilePage'
 import InstallPage from './pages/InstallPage'
 import SettingsPage from './pages/SettingsPage'
+import LandingPage from './pages/LandingPage'
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/app" element={<Layout />}>
         <Route index element={<Dashboard />} />
         <Route path="profile/:profileId" element={<ProfilePage />} />
         <Route path="profile/:profileId/install" element={<InstallPage />} />
