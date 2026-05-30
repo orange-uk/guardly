4a52a27be919bc68ecc926eec44af5c1458dbae8
orange-uk <93472932+orange-uk@users.noreply.github.com>
2026-05-30 01:35:12 +0100

    Add OnboardingFlow route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import ProfilePage from './pages/ProfilePage'
 import InstallPage from './pages/InstallPage'
 import SettingsPage from './pages/SettingsPage'
 import LandingPage from './pages/LandingPage'
+import OnboardingFlow from './onboarding/OnboardingFlow'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/onboarding" element={<OnboardingFlow />} />
       <Route path="/app" element={<Layout />}>
         <Route index element={<Dashboard />} />
         <Route path="profile/:profileId" element={<ProfilePage />} />
